d5bd031368c1f96a7596d6ee684a33f8bc65568a
nandan <nandankopplu2002@gmail.com>
2022-09-01 20:56:44 +0530

    Reverse a linked list solution added

---
+++ Reverse_a_linked_list.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+// Initial Template for C++// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+// } Driver Code Ends
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution
+{
+public:
+    // Function to reverse a linked list.
+    struct Node *reverseList(struct Node *head)
+    {
+        struct Node *temp;
+        struct Node *cur;
+        if (head == NULL)
+        {
+            return head;
+        }
+        temp = NULL;
+        while (head != NULL)
+        {
+            cur = head;
+            head = head->next;
+            cur->next = temp;
+            temp = cur;
+        }
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T, n, l, firstdata;
+    cin >> T;
+
+    while (T--)
+    {
+        struct Node *head = NULL, *tail = NULL;
+
+        cin >> n;
+
+        cin >> firstdata;
+        head = new Node(firstdata);
+        tail = head;
+
+        for (int i = 1; i < n; i++)
+        {
+            cin >> l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        head = ob.reverseList(head);
+
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
