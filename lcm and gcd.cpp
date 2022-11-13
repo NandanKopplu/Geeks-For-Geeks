3316ea12df71b0a2821460b8ed99fc5817f6512d
nandan <nandankopplu2002@gmail.com>
2022-11-13 23:03:01 +0530

    LCM and GCD solution added

---
+++ lcm and gcd.cpp
@@ -0,0 +1,11 @@
+public:
+vector<long long> lcmAndGcd(long long A, long long B)
+{
+    vector<long long> ans;
+    ans.push_back((A * B) / __gcd(A, B));
+    ans.push_back(__gcd(A, B));
+
+    return ans;
+}
+}
+;
